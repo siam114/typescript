93cf486ce5d125c9b9aa2f5ac6cca9a89bf339dd
smsiam114 <smsiamislam598@gmail.com>
2026-08-21 17:23:57 +0600

    difference jd and ts

--- items.js
+++ items.js
@@ -1,4 +1,8 @@
 "use strict";
 let items = 'laptop';
+let student = ['siam', 'rohima'];
+let siam = 100;
 // items= 100;
 console.log(items);
+console.log(student);
+console.log(siam);
--- items.ts
+++ items.ts
@@ -1,5 +1,9 @@
 let items = 'laptop';
+let student = ['siam','rohima']
+let siam = 100;
 
 // items= 100;
 
 console.log(items);
+console.log(student);
+console.log(siam);
