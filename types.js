0072d04cc322c46d6ea9d944792f1c2ddc122482
smsiam114 <smsiamislam598@gmail.com>
2026-08-21 17:34:35 +0600

    imtroduced types

---
+++ types.js
@@ -0,0 +1,5 @@
+"use strict";
+const district = "Dhaka";
+const stock = 100;
+const isActive = false;
+console.log(district);
---
+++ types.ts
@@ -0,0 +1,7 @@
+const district: string = "Dhaka"
+
+const stock: number = 100;
+
+const isActive: boolean = false
+
+console.log(district);
