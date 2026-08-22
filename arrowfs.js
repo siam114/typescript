66a60d2c6e8d5014d77b4be0e32db9b221bdfc1c
smsiam114 <smsiamislam598@gmail.com>
2026-08-22 21:37:56 +0600

    how to define arrow function is ts

--- arrowfs.js
+++ arrowfs.js
@@ -3,3 +3,11 @@ const add = (a, b) => {
     return a + b;
 };
 console.log(add(2, 3));
+const getLargerName = (name1, name2) => {
+    if (name1.length > name2.length) {
+        return name1;
+    }
+    return name2;
+};
+const biggerName = getLargerName('alis', 'smsiam');
+console.log(biggerName);
--- arrowfs.ts
+++ arrowfs.ts
@@ -3,3 +3,13 @@ const add = (a: number, b: number): number => {
 };
 
 console.log(add(2,3));
+
+const getLargerName = (name1: string, name2: string):string =>{
+    if(name1.length > name2.length){
+        return name1
+    }
+    return name2
+}
+
+const biggerName = getLargerName('alis', 'smsiam')
+console.log(biggerName);
