e3b9d7a6d23748280f0e13a648b859838eda6f25
smsiam114 <smsiamislam598@gmail.com>
2026-08-22 21:28:33 +0600

    more example for function

--- function.js
+++ function.js
@@ -3,3 +3,8 @@ function add(a, b) {
     return a + b;
 }
 console.log(add(2, 5));
+function greet(name, age) {
+    console.log(name, age);
+}
+console.log(greet('siam'));
+console.log(greet("Siam", 22));
--- function.ts
+++ function.ts
@@ -3,3 +3,11 @@ function add(a: number, b: number): number {
 }
 
 console.log(add(2,5));
+
+
+function greet(name: string, age?: number): void {
+  console.log(name, age);
+}
+
+console.log(greet('siam'));
+console.log(greet("Siam", 22));
