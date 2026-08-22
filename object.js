212a2c509d0cef078d2a7b5fed2260401a142e75
smsiam114 <smsiamislam598@gmail.com>
2026-08-22 20:50:07 +0600

    define object in the ts file

---
+++ object.js
@@ -0,0 +1,7 @@
+"use strict";
+const student = {
+    name: "Siam",
+    age: 22,
+    marks: 85
+};
+console.log(student);
---
+++ object.ts
@@ -0,0 +1,11 @@
+const student: {
+  name: string;
+  age: number;
+  marks: number;
+} = {
+  name: "Siam",
+  age: 22,
+  marks: 85
+};
+
+console.log(student);
